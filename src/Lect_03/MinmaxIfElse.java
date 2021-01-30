34ae2c5e8dec62c5793f800452f924756cfcd4a8
Maksim Biluta <biluta.m.a@gmail.com>
2021-01-30 23:54:05 +0100

    Update 28 Jan

--- src/Lect_03/MinmaxIfElse.java
+++ src/Lect_03/MinmaxIfElse.java
@@ -2,7 +2,7 @@ package Lect_03;
 
 import java.util.Scanner;
 
-public class Minmax_ternary {
+public class MinmaxIfElse {
     public static void main(String[] args) {
         System.out.println("*** Returns minimum of two numbers provided ***");
 
@@ -14,13 +14,12 @@ public class Minmax_ternary {
         System.out.print("Enter the second number: ");
         int number2 = scanner.nextInt();
 
-        if ((number1 != number2)) {
-            int maximumNumber = (number1 > number2) ? number1 : number2;
-            int minimumNumber = (number1 < number2) ? number1 : number2;
-            System.out.printf("%d is the biggest, %d is the smallest.", maximumNumber, minimumNumber);
-        } else {
+        if (number1 == number2) {
             System.out.println("Numbers are equal.");
+        } else if (number1 > number2) {
+            System.out.printf("%d is the biggest, %d is the smallest.", number1, number2);
+        } else {
+            System.out.printf("%d is the biggest, %d is the smallest.", number2, number1);
         }
     }
 }
-
--- src/Lect_03/Password.java
+++ src/Lect_03/Password.java
@@ -11,8 +11,18 @@ public class Password {
         String password = "ThePassword";
 
         System.out.print("Enter a password: ");
-        String userPassword = scanner.nextLine();
+        String passwordFromUser = scanner.nextLine();
 
-
+        if (password.length() == passwordFromUser.length()) {
+            if (password.equals(passwordFromUser)) {
+                System.out.println("Password ok.");
+            } else {
+                System.out.println("Password is wrong.");
+            }
+        } else if (password.length() > passwordFromUser.length()) {
+            System.out.println("Your password is too short.");
+        } else {
+            System.out.println("Your password is too long.");
+        }
     }
 }
