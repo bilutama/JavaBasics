c59742d83b6c2cde55c5095d12c339111a0d78e3
Maksim Biluta <biluta.m.a@gmail.com>
2021-01-31 21:30:42 +0100

    Update 31 Jan

--- src/JavaCourseTasks/NextDate.java
+++ src/JavaCourseTasks/NextDate.java
@@ -1,19 +1,47 @@
 package JavaCourseTasks;
 
+import java.util.Scanner;
+
 public class NextDate {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter date as D/M/YYYY");
+        int day = scanner.nextInt();
+
+        System.out.println("Enter date as D/M/YYYY");
+        int month = scanner.nextInt();
+
+        System.out.println("Enter date as D/M/YYYY");
+        int year = scanner.nextInt();
+
+        // Count days in months, index 0 corresponds to February in a leap year (29 days)
+        final int[] daysInMonths = new int[] {29, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     }
 
     private static int getDaysInYear (int year) {
-        int daysInYear = 366;
-
-        if (year % 4 != 0) {
-            daysInYear = 365;
-        } else if ((year % 100 != 0) && (year % 400 == 0)) {
-            daysInYear = 365;
+        if ((year % 4 == 0) && (year % 100 != 0)) {
+            return 366; // A leap year
+        } else if ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)) {
+            return 366; // A leap year
+        } else {
+            return 365; // NOT a leap year
         }
+    }
 
-        return daysInYear;
+    private static boolean isDateValid (int day, int month, int year) {
+        if  (year < 0 || month > 12 || month < 0 || day < 0) {
+            return false;
+        } else {
+            int monthIndex = month;
+
+            if (getDaysInYear(year) == 366 && month == 2) {
+                monthIndex = 0;
+            }
+
+            final int[] daysInMonths = new int[] {29, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+            return day <= daysInMonths[monthIndex];
+        }
     }
 }
