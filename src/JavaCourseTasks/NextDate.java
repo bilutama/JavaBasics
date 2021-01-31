16fb091a71ea300a8708a6ed9ac07458c0fcad54
Maksim Biluta <biluta.m.a@gmail.com>
2021-02-01 00:28:03 +0100

    Update 31 Jan

--- src/JavaCourseTasks/NextDate.java
+++ src/JavaCourseTasks/NextDate.java
@@ -18,8 +18,9 @@ public class NextDate {
         int year = scanner.nextInt();
 
         if (year < 0 || month > 12 || month < 0 || day < 0) {
-            System.out.println("Data is not valid");
+            System.out.println("Date is not valid.");
         } else {
+            // monthIndex is used to consider leap-year February while getting daysInMonth
             int monthIndex = month;
 
             if (isLeap(year) && month == 2) {
@@ -31,25 +32,25 @@ public class NextDate {
 
             if (day < daysInMonth[monthIndex]) {
                 // Date is valid and the day is NOT last in the month
-                System.out.printf("The next date is %02d.%02d.%d", (day + 1), month, year);
+                System.out.printf("The next day is %02d.%02d.%d", (day + 1), month, year);
             } else if (day == daysInMonth[monthIndex]) {
                 // Date is valid and the day IS the last in the month
                 if (month == 12) {
                     // It is December, 31
-                    System.out.printf("The next date is %02d.%02d.%d", 1, 1, (year + 1));
+                    System.out.printf("The next day is %02d.%02d.%d", 1, 1, (year + 1));
                 } else {
                     // It is NOT December, the last day in the month
-                    System.out.printf("The next date is %02d.%02d.%d", 1, (month + 1), year);
+                    System.out.printf("The next day is %02d.%02d.%d", 1, (month + 1), year);
                 }
             } else {
-                System.out.println("Data is not valid");
+                System.out.println("Date is not valid.");
             }
         }
     }
 
     private static boolean isLeap(int year) {
         if ((year % 4 == 0) && (year % 100 != 0)) {
-            return true; // A leap year
+            return true;
         } else {
             return (year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0);
         }
