420cafde1fd2532f8ba44c79dd3878a623448c24
Maksim Biluta <biluta.m.a@gmail.com>
2021-02-01 00:00:46 +0100

    Update 31 Jan

--- src/JavaCourseTasks/NextDate.java
+++ src/JavaCourseTasks/NextDate.java
@@ -31,15 +31,15 @@ public class NextDate {
 
             if (day < daysInMonth[monthIndex]) {
                 // Date is valid and the day is NOT last in the month
-                System.out.printf("The next date is %02d %02d %d", (day + 1), month, year);
+                System.out.printf("The next date is %02d.%02d.%d", (day + 1), month, year);
             } else if (day == daysInMonth[monthIndex]) {
                 // Date is valid and the day IS the last in the month
                 if (month == 12) {
                     // It is December, 31
-                    System.out.printf("The next date is %02d %02d %d", 1, 1, (year + 1));
+                    System.out.printf("The next date is %02d.%02d.%d", 1, 1, (year + 1));
                 } else {
                     // It is NOT December, the last day in the month
-                    System.out.printf("The next date is %02d %02d %d", 1, (month + 1), year);
+                    System.out.printf("The next date is %02d.%02d.%d", 1, (month + 1), year);
                 }
             } else {
                 System.out.println("Data is not valid");
