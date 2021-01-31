291eb746cce09375f0bc39a2dbf0582afec8842b
Maksim Biluta <biluta.m.a@gmail.com>
2021-01-31 11:05:30 +0100

    Update 31 Jan

---
+++ src/ClassTasks/CircleArea.java
@@ -0,0 +1,9 @@
+package ClassTasks;
+
+public class CircleArea {
+    public static void main(String[] args) {
+        double radius = 5.6;
+        double circleArea = Math.PI * Math.pow(radius, 2);
+        System.out.printf("Circle area is %f", circleArea);
+    }
+}
---
+++ src/JavaCourseTasks/Age.java
@@ -0,0 +1,33 @@
+package JavaCourseTasks;
+
+import java.lang.reflect.GenericDeclaration;
+import java.util.Scanner;
+
+public class Age {
+    public static void main(String[] args) {
+        System.out.println("*** Checks your age ***");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите ваш возраст: ");
+        int age = scanner.nextInt();
+
+        if (age > 0 && age <= 112) {
+            String ageTextEnding;
+
+            if ((age / 10) % 10 == 1) {
+                ageTextEnding = "лет";
+            } else if (age % 10 > 4) {
+                ageTextEnding = "лет";
+            } else {
+                ageTextEnding = "года";
+            }
+
+            System.out.printf("Вам %d %s", age, ageTextEnding);
+        } else if (age <= 0) {
+            System.out.println("Вы слишком малы.");
+        } else {
+            System.out.println("Вы слишком стары.");
+        }
+    }
+}
