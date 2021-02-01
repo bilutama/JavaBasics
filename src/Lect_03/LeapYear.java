41c051c2c0d23dd99fb6d1109d93c9feb8837e35
Maxim Biluta <78162367+bilutama@users.noreply.github.com>
2021-02-01 09:27:59 +0100

    Update LeapYear.java

--- src/Lect_03/LeapYear.java
+++ src/Lect_03/LeapYear.java
@@ -15,12 +15,12 @@ public class LeapYear {
                 System.out.print("Enter a year (value > 0 or 0 to terminate): ");
                 year = scanner.nextInt();
 
-                if (year == 0) System.exit(0);
+                if (year == 0) {
+                    System.exit(0);
+                }
             }
 
-            if ((year % 4 == 0) && (year % 100 != 0)) {
-                System.out.printf("%d is a leap year.%n", year);
-            } else if ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)) {
+            if (((year % 4 == 0) && (year % 100 != 0)) || ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0))) {
                 System.out.printf("%d is a leap year.%n", year);
             } else {
                 System.out.printf("%d is NOT a leap year.%n", year);
