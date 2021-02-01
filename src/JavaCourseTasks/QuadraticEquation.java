bbcc5cf99db69d4d4c16701e9cf3334613ddeb93
Maksim Biluta <biluta.m.a@gmail.com>
2021-02-01 20:56:13 +0100

    Update 31 Jan

--- src/JavaCourseTasks/QuadraticEquation.java
+++ src/JavaCourseTasks/QuadraticEquation.java
@@ -23,23 +23,23 @@ public class QuadraticEquation {
                 if (c == 0) {
                     System.out.println("There is no equation. Identity 0 == 0 is correct.");
                 } else {
-                    System.out.printf("Incorrect identity, because %f != 0%n.", c);
+                    System.out.printf("Incorrect identity, because %f != 0.%n", c);
                 }
             } else {
                 double root = -c / b;
                 System.out.printf("The equation is linear, x = -c / b = %f", root);
             }
         } else {
-            double discriminant = Math.pow(b, 2) - 4 * a * c;
             final double epsilon = 1.0E-10;
+            double discriminant = Math.pow(b, 2) - 4 * a * c;
 
             if (discriminant > epsilon) {
                 double equationRoot1 = (-b - Math.sqrt(discriminant)) / (2 * a);
                 double equationRoot2 = (-b + Math.sqrt(discriminant)) / (2 * a);
-                System.out.printf("The equation has two roots: x1 = %f, x2 = %f", equationRoot1, equationRoot2);
+                System.out.printf("The equation has two roots: x1 = %f, x2 = %f.%n", equationRoot1, equationRoot2);
             } else if (Math.abs(discriminant) <= epsilon) {
                 double equationRoot = -b / (2 * a);
-                System.out.printf("The equation has one root x = %f", equationRoot);
+                System.out.printf("The equation has one root x = %f.%n", equationRoot);
             } else {
                 System.out.println("The equation has no roots.");
             }
