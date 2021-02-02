3da82016544d911ba9ddc88986650079e68becb2
Maxim Biluta <78162367+bilutama@users.noreply.github.com>
2021-02-02 14:58:54 +0100

    Update NextDate.java

--- src/JavaCourseTasks/NextDate.java
+++ src/JavaCourseTasks/NextDate.java
@@ -6,38 +6,50 @@ public class NextDate {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter date as D/M/YYYY");
+        System.out.println("Enter date as D/M/YYYY (0 to terminate)");
 
         System.out.print("Day: ");
         int day = scanner.nextInt();
+        
+        if (day == 0) {
+            return;
+        }
 
         System.out.print("Month: ");
         int month = scanner.nextInt();
 
+        if (month == 0) {
+            return;
+        }
+        
         System.out.print("Year: ");
         int year = scanner.nextInt();
+        
+        if (year == 0) {
+            return;
+        }
 
         if (year < 0 || month > 12 || month < 0 || day < 0) {
             System.out.println("Date is not valid.");
         } else {
-            int daysInMonth;
+            int daysCountInMonth;
             
             if (month == 2) {
                 if (isLeap(year)) {
-                    daysInMonth = 29;
+                    daysCountInMonth = 29;
                 } else {
-                    daysInMonth = 28;
+                    daysCountInMonth = 28;
                 }
             } else if (month == 4 || month == 6 || month == 9 || month == 11) {
-                daysInMonth = 30;
+                daysCountInMonth = 30;
             } else {
-                daysInMonth = 31;
+                daysCountInMonth = 31;
             }
                     
-            if (day < daysInMonth) {
+            if (day < daysCountInMonth) {
                 // Date is valid and the day is NOT last in the month
                 System.out.printf("The next day is %02d.%02d.%d", (day + 1), month, year);
-            } else if (day == daysInMonth) {
+            } else if (day == daysCountInMonth) {
                 // Date is valid and the day IS the last in the month
                 if (month == 12) {
                     // It is December, 31
