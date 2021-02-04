47637dcc043fe0f5650ffc3d3511e4582875c5ac
Maxim Biluta <78162367+bilutama@users.noreply.github.com>
2021-02-04 14:13:36 +0100

    Update FibonacciMatrix.java

--- src/JavaCourseTasks/FibonacciMatrix.java
+++ src/JavaCourseTasks/FibonacciMatrix.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 
 public class FibonacciMatrix {
     public static void main(String[] args) throws Exception {
-        System.out.println("*** Returns Fibonacci number by index ***");
-
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("*** Returns Fibonacci number by index ***");
+
         int fibonacciNumberIndex = 0;
-        while (fibonacciNumberIndex < 1 || fibonacciNumberIndex > 46) {
-            System.out.print("Enter an index (> 0 and < 47): ");
+        while (fibonacciNumberIndex < 1 || fibonacciNumberIndex > 92) {
+            System.out.print("Enter an index (> 0 and < 93): ");
             fibonacciNumberIndex = scanner.nextInt();
         }
 
         long algorithmStartTime = System.currentTimeMillis();
 
-        int[][] basicMatrix = {
-                {1, 1},
-                {1, 0}
+        long[][] basicMatrix = {
+                {1L, 1L},
+                {1L, 0L}
         };
 
         try {
-            int[][] fibonacciMatrix = getMatrixInPower(basicMatrix, fibonacciNumberIndex);
+            long[][] fibonacciMatrix = getMatrixInPower(basicMatrix, fibonacciNumberIndex);
             System.out.printf("Number #%d = %d%n", fibonacciNumberIndex, fibonacciMatrix[0][1]);
         }
         catch (Exception exception) {
@@ -33,12 +33,12 @@ public class FibonacciMatrix {
         System.out.printf("< Executed in %dms >%n", algorithmEndTime - algorithmStartTime);
     }
 
-    public static int[][] getMatricesMultiplication(int[][] matrix1, int[][] matrix2) throws Exception {
+    public static long[][] getMatricesMultiplication(long[][] matrix1, long[][] matrix2) throws Exception {
         if (matrix1[0].length != matrix2.length) {
             throw new Exception("ERROR: multiplication of matrices is impossible");
         }
 
-        int[][] matrixProduct = new int[matrix1.length][matrix2[0].length];
+        long[][] matrixProduct = new long[matrix1.length][matrix2[0].length];
 
         for (int i = 0; i < matrix1.length; i++) {
             for (int j = 0; j < matrix2[0].length; j++) {
@@ -50,32 +50,32 @@ public class FibonacciMatrix {
         return matrixProduct;
     }
 
-    public static int[][] getMatrixInPower(int[][] matrix, int n) throws Exception {
+    public static long[][] getMatrixInPower(long[][] matrix, int n) throws Exception {
         if (matrix.length != matrix[0].length) {
             throw new Exception("ERROR: impossible to power the matrix");
         }
 
-        int[][] matrixInPower;
+        long[][] matrixInPower;
 
         if (n == 0) {
-            matrixInPower = new int[matrix.length][matrix.length];
+            matrixInPower = new long[matrix.length][matrix.length];
 
             for (int i = 0; i < matrix.length; i++) {
                 matrixInPower[i][i] = 1;
             }
         } else {
             if (n % 2 == 0) {
-                int[][] temporaryMatrix = getMatrixInPower(matrix, n / 2);
-                return getMatricesMultiplication(temporaryMatrix, temporaryMatrix);
+                long[][] temporaryMatrix = getMatrixInPower(matrix, n / 2);
+                matrixInPower = getMatricesMultiplication(temporaryMatrix, temporaryMatrix);
             } else {
-                int[][] temporaryMatrix = getMatrixInPower(matrix, (n - 1) / 2);
-                return getMatricesMultiplication(matrix, getMatricesMultiplication(temporaryMatrix, temporaryMatrix));
+                long[][] temporaryMatrix = getMatrixInPower(matrix, (n - 1) / 2);
+                matrixInPower = getMatricesMultiplication(matrix, getMatricesMultiplication(temporaryMatrix, temporaryMatrix));
             }
         }
         return matrixInPower;
     }
 
-    public static void printMatrix(int[][] matrix) {
+    public static void printMatrix(long[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 System.out.printf("%10d", matrix[i][j]);
