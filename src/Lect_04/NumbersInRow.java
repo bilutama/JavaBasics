bf4a43d925ce5509ed4155610155d1e082cb54aa
Maxim Biluta <78162367+bilutama@users.noreply.github.com>
2021-02-04 09:27:57 +0100

    Update NumbersInRow.java

--- src/Lect_04/NumbersInRow.java
+++ src/Lect_04/NumbersInRow.java
@@ -9,12 +9,14 @@ public class NumbersInRow {
         Scanner scanner = new Scanner(System.in);
 
         int firstNumber = 0;
+        
         while (firstNumber < 1) {
             System.out.print("The first number to print (> 0): ");
             firstNumber = scanner.nextInt();
         }
 
         int lastNumber = 0;
+        
         while (lastNumber < 1) {
             System.out.print("The last number to print (> 0): ");
             lastNumber = scanner.nextInt();
@@ -27,6 +29,7 @@ public class NumbersInRow {
         }
 
         int numbersCountInRow = 0;
+        
         while (numbersCountInRow < 1) {
             System.out.print("How many numbers in a row (> 0): ");
             numbersCountInRow = scanner.nextInt();
