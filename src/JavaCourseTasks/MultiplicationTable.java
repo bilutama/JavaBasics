2b3d3fbcbaccae7a71b66be5bab83972e20fcd2a
Maksim Biluta <biluta.m.a@gmail.com>
2021-02-04 21:56:44 +0100

    Update 4 Feb Lect 04

--- src/JavaCourseTasks/MultiplicationTable.java
+++ src/JavaCourseTasks/MultiplicationTable.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        System.out.println("*** Prints numbers in rows ***");
+        System.out.println("*** Prints multiplication table ***");
 
         Scanner scanner = new Scanner(System.in);
 
