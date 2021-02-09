7fe8052b6266a0697569ed68cbade05fc7932855
Maxim Biluta <78162367+bilutama@users.noreply.github.com>
2021-02-09 12:17:17 +0100

    Update MultiplicationTable.java

--- src/JavaCourseTasks/MultiplicationTable.java
+++ src/JavaCourseTasks/MultiplicationTable.java
@@ -8,46 +8,42 @@ public class MultiplicationTable {
 
         Scanner scanner = new Scanner(System.in);
 
-        int rowCount = 0;
+        int rowsCount = 0;
 
-        while (rowCount < 2) {
+        while (rowsCount < 2) {
             System.out.print("Row count (> 1): ");
-            rowCount = scanner.nextInt();
+            rowsCount = scanner.nextInt();
         }
 
-        int columnCount = 0;
+        int columnsCount = 0;
 
-        while (columnCount < 2) {
+        while (columnsCount < 2) {
             System.out.print("Column count (> 1): ");
-            columnCount = scanner.nextInt();
+            columnsCount = scanner.nextInt();
         }
 
         // printing the first line
-        for (int i = 1; i <= columnCount; i++) {
-            if (i > 1) {
-                System.out.printf("%4d", i);
-            } else {
-                System.out.print("    ");
-            }
+        System.out.print("    ");
+        
+        for (int i = 2; i <= columnsCount; i++) {
+            System.out.printf("%4d", i);
         }
 
         System.out.println();
 
         // printing the separator
-        for (int i = 1; i <= columnCount; i++) {
+        for (int i = 1; i <= columnsCount; i++) {
             System.out.print("____");
         }
 
         System.out.println();
 
         // printing the multiplication table
-        for (int i = 2; i <= rowCount; i++) {
-            for (int j = 1; j <= columnCount; j++) {
-                if (j == 1) {
-                    System.out.printf("%3d|", i);
-                } else {
-                    System.out.printf("%4d", i * j);
-                }
+        for (int i = 2; i <= rowsCount; i++) {
+            System.out.printf("%3d|", i);
+            
+            for (int j = 1; j <= columnsCount; j++) {
+                System.out.printf("%4d", i * j);
             }
 
             System.out.println();
