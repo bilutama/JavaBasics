3aab0adeb750ac1c509e6ccfefd575ec91b0db95
Maksim Biluta <biluta.m.a@gmail.com>
2021-02-09 20:21:22 +0100

    Update 4 Feb Lect 04

--- src/JavaCourseTasks/FibonacciDirectSum.java
+++ src/JavaCourseTasks/FibonacciDirectSum.java
@@ -8,11 +8,30 @@ public class FibonacciDirectSum {
 
         System.out.println("*** Finds Fibonacci numbers ***");
         
-        System.out.print("Enter an index (> 0 and < 93): ");
+        System.out.print("Enter an index (< 93): ");
         int fibonacciIndex = scanner.nextInt();
 
-        long algorithmStartTime = System.currentTimeMillis();
+        if (fibonacciIndex == 0 || fibonacciIndex == 1) {
+            System.out.printf("Number #%d = %d%n", fibonacciIndex, fibonacciIndex);
+        } else if (fibonacciIndex > 92) {
+            System.out.println("Invalid index.");
+        } else {
+            long fibonacciNumber1 = 0;
+            long fibonacciNumber2 = 1;
+            long fibonacciNumber3 = 1;
 
+            int i = 2;
+            while (i != fibonacciIndex) {
+                fibonacciNumber1 = fibonacciNumber2;
+                fibonacciNumber2 = fibonacciNumber3;
+                fibonacciNumber3 = fibonacciNumber2 + fibonacciNumber1;
+                ++i;
+            }
+
+            System.out.printf("Number #%d = %,d%n", fibonacciIndex, fibonacciNumber3);
+        }
+
+/*
         if (fibonacciIndex > 0 && fibonacciIndex < 93) {
             long[] fibonacciNumbers = new long[2];
             fibonacciNumbers[1] = 1L;
@@ -35,5 +54,7 @@ public class FibonacciDirectSum {
 
         long algorithmEndTime = System.currentTimeMillis();
         System.out.printf("< Executed in %dms >%n", algorithmEndTime - algorithmStartTime);
+
+ */
     }
 }
