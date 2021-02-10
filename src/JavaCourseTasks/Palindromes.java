3e99831efb4bf5c927b8e6dbb9f42b4b6e207f62
Maksim Biluta <biluta.m.a@gmail.com>
2021-02-10 02:07:19 +0100

    Update 4 Feb Lect 04

--- src/JavaCourseTasks/Palindromes.java
+++ src/JavaCourseTasks/Palindromes.java
@@ -20,49 +20,30 @@ public class Palindromes {
                 System.out.println("No palindromes for the string.");
             }
         } else {
-            int palindromeMaximumLength = 0;
-
             for (int charIndex = 1; charIndex < string.length() - 1; ++charIndex) {
-                if ((string.charAt(charIndex) == string.charAt(charIndex - 1)) && (string.charAt(charIndex) != string.charAt(charIndex + 1))) {
-                    // EVEN PALINDROME CENTRE 'aa'
-                    int left = charIndex - 1;
-                    int right = charIndex;
-
-                    while (left > 0 && right < string.length() - 1) {
-                        if (string.charAt(left - 1) == string.charAt(right + 1)) {
-                            --left;
-                            ++right;
-                            continue;
-                        }
-                        break;
-                    }
-
-                    String palindrome = string.substring(left, right + 1);
-                    System.out.printf("Palindrome: %s%n", palindrome);
-
-                } else if (string.charAt(charIndex - 1) == string.charAt(charIndex + 1)) {
-                    // ODD PALINDROME CENTRE 'aba'
-                    int left = charIndex - 1;
-                    int right = charIndex + 1;
-
-                    while (left > 0 && right < string.length() - 1) {
-                        if (string.charAt(left - 1) == string.charAt(right + 1)) {
-                            --left;
-                            ++right;
-                            continue;
-                        }
-                        break;
-                    }
-
-                    String palindrome = string.substring(left, right + 1);
-                    System.out.printf("Palindrome: %s%n", palindrome);
+                if ((string.charAt(charIndex) != string.charAt(charIndex - 1)) && (string.charAt(charIndex + 1) != string.charAt(charIndex - 1))) {
+                    continue;
                 }
+
+                int left = charIndex - 1;
+                int right = charIndex;
+
+                if (string.charAt(charIndex - 1) == string.charAt(charIndex + 1)) {
+                    right = charIndex + 1;
+                }
+
+                while (left > 0 && right < string.length() - 1) {
+                    if (string.charAt(left - 1) == string.charAt(right + 1)) {
+                        --left;
+                        ++right;
+                        continue;
+                    }
+                    break;
+                }
+
+                String palindrome = string.substring(left, right + 1);
+                System.out.printf("Palindrome: %s%n", palindrome);
             }
         }
-    }
-
-    private boolean isPalindrome(String string){
-
-        return true;
     }
 }
