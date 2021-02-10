6bad46d74ef2afd8ab586ee23032be12991e727b
Maxim Biluta <78162367+bilutama@users.noreply.github.com>
2021-02-10 14:20:05 +0100

    Update CyclesClassTasks.java

--- src/ClassTasks/CyclesClassTasks.java
+++ src/ClassTasks/CyclesClassTasks.java
@@ -2,18 +2,30 @@ package ClassTasks;
 
 public class CyclesClassTasks {
     public static void main(String[] args) {
+        // TASK 1 ==================================
         for (int i = 100; i >= 0; --i) {
             if (i % 4 == 0) {
                 System.out.printf("%d%n", i);
             }
         }
         
+        // TASK 2 ==================================
         int n = 5;
         
         for (int i = 1; i <= n; ++i) {
             System.out.printf("%d%n", i*i);
         }
 
+        // TASK 3 ==================================
         
+        int x = 4;
+        int y = 10;
+        int sum = 0;
+        
+        for (int i = x; i <= y; ++i) {
+            sum += i;
+        }
+
+        double average = (double) sum / (y - x + 1);
     }
 }
