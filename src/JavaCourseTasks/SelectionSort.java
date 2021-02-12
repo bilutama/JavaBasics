cc1f61aa033a57b0f1d2e7326c1eefc5afff6579
Maxim Biluta <78162367+bilutama@users.noreply.github.com>
2021-02-12 16:03:05 +0100

    Update SelectionSort.java

--- src/JavaCourseTasks/SelectionSort.java
+++ src/JavaCourseTasks/SelectionSort.java
@@ -1,25 +1,46 @@
 package JavaCourseTasks;
 
 public class SelectionSort {
-    public static void main(String[] args) {
-        final int arraySize = 10;
+    	public static void main(String[] args) {
+        final int arraySize = 20;
         int[] array = new int[arraySize];
 
         // Array initialization
         for (int i = 0; i < arraySize; ++i) {
-            array[i] = 2 * (i + 1);
+            array[i] = arraySize - i;
         }
 
-        // printing the array;
-        final int numbersCountInRow = 10;
-        System.out.println("The array is:");
+        // printing the array
+        System.out.println("The initial array:");
 
-        for (int i = 0; i < array.length; i++) {
-            System.out.printf("%4d", array[i]);
-
-            if ((i + 1) % numbersCountInRow == 0) {
-                System.out.println();
+        for(int i : array) {
+            System.out.printf("%3d", i);
+        }
+        
+        System.out.println();
+        
+        // sorting the array
+        for (int i = 0; i < arraySize - 1; ++i) {
+            int minIndex = i;
+            
+            for (int j = i + 1; j < arraySize; ++j) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
             }
+            
+            if (array[i] > array[minIndex]) {
+                int temp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = temp;
+            }
+        }
+        
+        // printing the sorted array
+        System.out.println("The sorted array:");
+        
+        for(int i : array) {
+            System.out.printf("%3d", i);
         }
     }
 }
