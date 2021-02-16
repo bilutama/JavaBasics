78300afb6118d2ab4cf99b7ce342914487661ab6
Maksim Biluta <biluta.m.a@gmail.com>
2021-02-17 00:59:05 +0100

    Update 4 Feb Lect 04

--- src/JavaCourseTasks/HeapSort.java
+++ src/JavaCourseTasks/HeapSort.java
@@ -2,6 +2,54 @@ package JavaCourseTasks;
 
 public class HeapSort {
     public static void main(String[] args) {
+        final int arraySize = 10;
+        int[] array = new int[arraySize];
+
+        // Array initialization
+        for (int i = 0; i < arraySize; ++i) {
+            //array[i] = arraySize - i;
+            array[i] = i + 1;
+        }
+
+        // printing the array
+        System.out.println("The initial array:");
+
+        for (int i : array) {
+            System.out.printf("%3d", i);
+        }
+
+        System.out.println();
+
+        // Build a heap from the array
+        for (int i = arraySize / 2 - 1; i >= 0; --i) {
+
+            int leftChild = 2 * i + 1;
+            int rightChild = 2 * i + 2;
+
+            int max = i;
+
+            if (leftChild < arraySize && array[leftChild] > array[i]) {
+                max = leftChild;
+            }
+
+            if (rightChild < arraySize && array[rightChild] > array[max]) {
+                max = rightChild;
+            }
+
+            // move max element to the root
+            if (i != max) {
+                int temp = array[max];
+                array[max] = array[i];
+                array[i] = temp;
+            }
+
+        }
+
+        for (int i : array) {
+            System.out.printf("%3d", i);
+        }
+
+        // Sort the heap recursively
 
     }
 }
--- src/JavaCourseTasks/SelectionSort.java
+++ src/JavaCourseTasks/SelectionSort.java
@@ -1,7 +1,7 @@
 package JavaCourseTasks;
 
 public class SelectionSort {
-    	public static void main(String[] args) {
+    public static void main(String[] args) {
         final int arraySize = 20;
         int[] array = new int[arraySize];
 
@@ -13,33 +13,33 @@ public class SelectionSort {
         // printing the array
         System.out.println("The initial array:");
 
-        for(int i : array) {
+        for (int i : array) {
             System.out.printf("%3d", i);
         }
-        
+
         System.out.println();
-        
+
         // sorting the array
         for (int i = 0; i < arraySize - 1; ++i) {
             int minIndex = i;
-            
+
             for (int j = i + 1; j < arraySize; ++j) {
                 if (array[j] < array[minIndex]) {
                     minIndex = j;
                 }
             }
-            
+
             if (array[i] > array[minIndex]) {
                 int temp = array[minIndex];
                 array[minIndex] = array[i];
                 array[i] = temp;
             }
         }
-        
+
         // printing the sorted array
         System.out.println("The sorted array:");
-        
-        for(int i : array) {
+
+        for (int i : array) {
             System.out.printf("%3d", i);
         }
     }
