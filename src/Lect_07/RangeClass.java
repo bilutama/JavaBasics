124ccd9912a9d0b6f6aab2bf6ff71c314a07c8d6
Maxim Biluta <78162367+bilutama@users.noreply.github.com>
2021-02-17 11:54:23 +0100

    Update RangeClass.java

--- src/Lect_07/RangeClass.java
+++ src/Lect_07/RangeClass.java
@@ -4,29 +4,29 @@ public class RangeClass {
     private double from;
     private double to;
 
-    public RangeClass(double from, double to) {
+    public Range(double from, double to) {
         this.from = from;
-        this.to = to;
-    }
-
-    public void setFrom(double from) {
-        this.from = from;
-    }
-
-    public void setTo(double to) {
         this.to = to;
     }
 
     public double getFrom() {
         return from;
     }
+    
+    public void setFrom(double from) {
+        this.from = from;
+    }
 
-    public double getTo() {
+     public double getTo() {
         return to;
     }
 
-    public double getRangeLength() {
-        return from - to;
+    public void setTo(double to) {
+        this.to = to;
+    }
+    
+    public double getLength() {
+        return to - from;
     }
 
     public boolean isInside(double point) {
