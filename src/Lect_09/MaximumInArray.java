607ab7364c9116fbd76ea0578d0a36d7d5faf755
Maksim Biluta <biluta.m.a@gmail.com>
2021-02-23 20:02:11 +0100

    Update 4 Feb Lect 04

--- src/Lect_09/MaximumInArray.java
+++ src/Lect_09/MaximumInArray.java
@@ -2,14 +2,14 @@ package Lect_09;
 
 public class MaximumInArray {
     public static void main(String[] args) {
-        double[] array = new double[]{1.4, 2.5, 3.7, 4.3, 10.9, 5.3, 6.3, 7.6, 8.1, 5.2};
+        double[] array = {1.4, 2.5, 3.7, 4.3, 10.9, 5.3, 6.3, 7.6, 8.1, 5.2};
 
         System.out.println("Array is:");
         printArray(array);
-        System.out.printf("It's maximum element is %.1f", getMaximumIndex(array));
+        System.out.printf("It's maximum element is %.1f", getMaximumElementIndex(array));
     }
 
-    public static double getMaximumIndex(double[] array) {
+    public static double getMaximumElementIndex(double[] array) {
         double max = array[0];
 
         for (int i = 1; i < array.length; ++i) {
