55a2e00126684a09ff682eff1875273fb60a2c41
Maksim Biluta <biluta.m.a@gmail.com>
2021-02-28 01:20:27 +0100

    Update 4 Feb Lect 04

--- src/JavaCourseTasks/QuickSort.java
+++ src/JavaCourseTasks/QuickSort.java
@@ -2,24 +2,58 @@ package JavaCourseTasks;
 
 public class QuickSort {
     public static void main(String[] args) {
-        final int arraySize = 5;
-        int[] array = new int[arraySize];
+        final int ARRAY_SIZE = 15;
+        int[] array = new int[ARRAY_SIZE];
 
-        // Array initialization
-        for (int i = 0; i < arraySize; ++i) {
-            array[i] = arraySize - i;
+        for (int i = 0; i < ARRAY_SIZE; ++i) {
+            array[i] = ARRAY_SIZE - i;
         }
+
+        //int[] array = {4, 1, 3, 2};
 
         System.out.println("The initial array:");
         printArray(array);
 
-        // sorting the array
-        for (int i = 0; i < arraySize - 1; ++i) {
-            // TODO: implement quicksort algorithm
-        }
+        System.out.println("Sorting:");
+        sortArray(array, 0, array.length - 1);
 
         System.out.println("The sorted array:");
         printArray(array);
+    }
+
+    private static void sortArray(int[] array, int left, int right) {
+        //int reference = left + (right - left) / 2;
+        int reference = array[left + (right - left) / 2];
+
+        int i = left;
+        int j = right;
+
+        while (i <= j) {
+            while (array[i] < reference) {
+                ++i;
+            }
+
+            while (array[j] > reference) {
+                --j;
+            }
+
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                ++i;
+                --j;
+                //printArray(array);
+            }
+        }
+
+        if (i < right) {
+            sortArray(array, i, right);
+        }
+
+        if (j > left) {
+            sortArray(array, left, j);
+        }
     }
 
     static private void printArray(int[] array) {
