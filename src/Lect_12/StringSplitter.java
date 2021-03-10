314bea76162876f5899ed33e05f047affe856899
bilutama <Borntobewild282>
2021-03-10 01:03:42 +0100

    regular commit

--- src/Lect_12/StringSplitter.java
+++ src/Lect_12/StringSplitter.java
@@ -1,9 +1,21 @@
 package Lect_12;
 
 public class StringSplitter {
-    public static void main(String[] args) {
-        // Разбить строку “1, 2, 3, 4, 5” и получить массив из
-        // этих чисел и найти их сумму
-        // Использовать split и Integer .parseInt
+    public static void main(String[] args) throws Exception {
+        String string = "1, 2, 3, 4, 5";
+
+        String[] stringArray = string.split(",");
+        int[] numbers = new int[stringArray.length];
+        int sum = 0;
+
+        try {
+            for (String s : stringArray) {
+                sum += Integer.parseInt(s.trim());
+            }
+
+            System.out.println(sum);
+        } catch (Exception exception) {
+            System.out.println("Unable to parse numbers from the string");
+        }
     }
 }
