0fc2353bdca6736f78b02b0dc8258cd7ef72bd7c
bilutama <Borntobewild282>
2021-03-14 18:38:23 +0100

    regular commit

--- src/Extra/MatrixDeterminant.java
+++ src/Extra/MatrixDeterminant.java
@@ -3,63 +3,53 @@ package Extra;
 public class MatrixDeterminant {
     public static void main(String[] args) {
         int[][] matrix = {
-                {5, 1, 1},
-                {1, 3, 1},
-                {7, 8, 9}
+                {1, -2, 3},
+                {4, 8, 6},
+                {-7, 8, 9}
         };
 
-        System.out.println(getMatrixDeterminant(matrix));
-        //printMatrix(getSubmatrix(matrix, 0, 1));
+        printMatrix(matrix);
+        System.out.printf("Determinant = %d%n", getMatrixDeterminant(matrix));
     }
 
     public static int getMatrixDeterminant(int[][] matrix) {
-        int determinant = 0;
-
-        // Определитель для элементарной матрицы
         if (matrix.length == 1) {
             return matrix[0][0];
         }
 
+        // Индекс столбца матрицы, по которой считается определитель
+        final int REFERENCE_COLUMN_INDEX = 0;
+        int determinant = 0;
+
         for (int i = 0; i < matrix.length; ++i) {
-            determinant += (1 - 2 * (i % 2)) * matrix[i][0] * getMatrixDeterminant(getSubmatrix(matrix, i, 0));
-            //determinant += matrix[i][0] * getMatrixDeterminant(getSubmatrix(matrix, i, 0));
+            determinant += (1 - 2 * (i % 2)) * matrix[i][REFERENCE_COLUMN_INDEX] * getMatrixDeterminant(getMinorMatrix(matrix, i, REFERENCE_COLUMN_INDEX));
         }
-        //System.out.println(determinant);
+
         return determinant;
     }
 
-    public static int[][] getSubmatrix(int[][] matrix, int excludedRowIndex, int excludedColumnIndex) {
+    // Вычисление минора основной матрицы
+    public static int[][] getMinorMatrix(int[][] matrix, int excludedRowIndex, int excludedColumnIndex) {
         int[][] subMatrix = new int[matrix[0].length - 1][matrix[0].length - 1];
 
-        int shiftIndexRow = 0;
-        int shiftIndexColumn = 0;
-
         for (int i = 0; i < matrix.length; ++i) {
-            // Пропуск строки и вычисление сдвига индекса по строке
+            // Пропуск референсной строки
             if (i == excludedRowIndex) {
                 continue;
             }
 
-            if (i > excludedRowIndex) {
-                shiftIndexRow = -1;
-            } else {
-                shiftIndexRow = 0;
-            }
+            // Вычисление сдвига индекса по строке
+            int rowIndexShift = i > excludedRowIndex ? -1 : 0;
 
             for (int j = 0; j < matrix[0].length; ++j) {
-
-                // Пропуск столбца и вычисление сдвига индекса по столбцу
+                // Пропуск референсного столбца
                 if (j == excludedColumnIndex) {
                     continue;
                 }
 
-                if (j > excludedColumnIndex) {
-                    shiftIndexColumn = -1;
-                } else {
-                    shiftIndexColumn = 0;
-                }
-
-                subMatrix[i + shiftIndexRow][j + shiftIndexColumn] = matrix[i][j];
+                // Вычисление сдвига индекса по столбцу
+                int columnIndexShift = j > excludedColumnIndex ? -1 : 0;
+                subMatrix[i + rowIndexShift][j + columnIndexShift] = matrix[i][j];
             }
         }
 
@@ -67,9 +57,11 @@ public class MatrixDeterminant {
     }
 
     public static void printMatrix(int[][] matrix) {
-        for (int i = 0; i < matrix.length; ++i) {
+        System.out.println("Matrix:");
+
+        for (int[] element : matrix) {
             for (int j = 0; j < matrix[0].length; ++j) {
-                System.out.printf("%4d", matrix[i][j]);
+                System.out.printf("%3d", element[j]);
             }
 
             System.out.println();
