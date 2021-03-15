db3e9ab9f8eb55a7fa17c6335d4e916ac6b020c1
Maksim Biluta <biluta.m.a@gmail.com>
2021-03-16 00:44:34 +0100

    new commit

--- src/Lect_13/ImageBlurring.java
+++ src/Lect_13/ImageBlurring.java
@@ -9,78 +9,95 @@ import java.io.IOException;
 public class ImageBlurring {
     public static void main(String[] args) throws IOException {
         // читаем картинку из файлу image.jpg в объект класса BufferedImage
-        BufferedImage imageInput = ImageIO.read(new File("image.jpg"));
+        BufferedImage inputImage = ImageIO.read(new File("image.jpg"));
 
         // получаем растр - объект, внутри которого содержится двумерный массив пикселей
-        WritableRaster rasterInput = imageInput.getRaster();
+        WritableRaster inputRaster = inputImage.getRaster();
 
         // получаем ширину и высоту картинки
-        int width = rasterInput.getWidth();
-        int height = rasterInput.getHeight();
+        int width = inputRaster.getWidth();
+        int height = inputRaster.getHeight();
 
         // Создаем новое изображение и растр, в который будем записывать результат,
         // той же размерности, что и исходное изображение
-        BufferedImage imageOutput = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        WritableRaster rasterOutput = imageOutput.getRaster();
+        BufferedImage outputImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        WritableRaster outputRaster = outputImage.getRaster();
 
         final int COLORS_COUNT_IN_RGB = 3;
-        //final int MAX_RGB = 255;
 
         // Степень размытия - значение, от которой вычисляется размерность матрицы размытия
         final int BLURRING_STRENGTH = 3;
 
         // Размерность матрицы размытия
         int blurringMatrixDimension = BLURRING_STRENGTH * 2 + 1;
-        
+
         double[][] blurringMatrix = new double[blurringMatrixDimension][blurringMatrixDimension];
 
         // Инициализация матрицы размытия
+        int normalizationFactor = blurringMatrixDimension * blurringMatrixDimension;
+
         for (int i = 0; i < blurringMatrixDimension; ++i) {
             for (int j = 0; j < blurringMatrixDimension; ++j) {
-                blurringMatrix[i][j] = 1.0 / (blurringMatrixDimension * blurringMatrixDimension);
+                blurringMatrix[i][j] = 1.0 / (normalizationFactor);
             }
         }
 
         // создаем массив, в котором будет содержаться текущий пиксель
         // это массив из 3 элементов, в нем по очереди лежат числа R, G, B
         // т.е. по индексу 0 будет лежать красная компонента, по индексу 1 - зеленая, по индексу 2 - синяя
-        int[] pixel = new int[COLORS_COUNT_IN_RGB];
-        int[] tempPixel = new int[COLORS_COUNT_IN_RGB];
+        int[] inputPixel = new int[COLORS_COUNT_IN_RGB];
+        int[] outputPixel = new int[COLORS_COUNT_IN_RGB];
 
-        // цикл по строкам картинки
-        for (int y = 0; y < height; ++y) {
-            // цикл пикселям строки
-            for (int x = 0; x < width; ++x) {
-                for (int k = 0; k < COLORS_COUNT_IN_RGB; ++k) {
-                    // Проверка на граничные пиксели - их не меняем, т.к. матрица размытия выйдет за пределы изображения
-                    if (y < blurringMatrixDimension / 2 + 1 || y >= height - blurringMatrixDimension / 2 ||
-                            x < blurringMatrixDimension / 2 + 1 || x >= width - blurringMatrixDimension / 2) {
-                        rasterInput.getPixel(x, y, tempPixel);
-                        pixel[k] = tempPixel[k];
-                    } else {
-                        double blurredPixel = 0.0;
+        // цикл по строке картинки
+        for (int x = 0; x < width; ++x) {
+            // цикл по столбцу картинки
+            for (int y = 0; y < height; ++y) {
+                // Проверка на граничные пиксели - их не меняем, т.к. матрица размытия выйдет за пределы изображения
+                if (x <= blurringMatrixDimension / 2 || x >= width - blurringMatrixDimension / 2 ||
+                        y <= blurringMatrixDimension / 2 || y >= height - blurringMatrixDimension / 2) {
+                    inputRaster.getPixel(x, y, outputPixel);
+                    outputRaster.setPixel(x, y, outputPixel);
+                    continue;
+                }
 
-                        // циклы по матрице размытия
-                        for (int i = 0; i < blurringMatrixDimension; ++i) {
-                            for (int j = 0; j < blurringMatrixDimension; ++j) {
-                                // получаем пиксели по матрице вокруг текущего с координатами (x, y)
-                                rasterInput.getPixel(x + i - blurringMatrixDimension / 2, y + j - blurringMatrixDimension / 2, tempPixel);
-                                // и суммируем с перемножением на коэффициенты матрицы размытия
-                                blurredPixel += tempPixel[k] * blurringMatrix[i][j];
-                            }
+                double[] intermediateOutputPixel = new double[COLORS_COUNT_IN_RGB];
+                int processedImageAreaLeftTopX = x - blurringMatrixDimension / 2;
+                int processedImageAreaLeftTopY = y - blurringMatrixDimension / 2;
+
+                // цикл по матрице размытия
+                for (int i = 0; i < blurringMatrixDimension; ++i) {
+                    for (int j = 0; j < blurringMatrixDimension; ++j) {
+                        inputRaster.getPixel(processedImageAreaLeftTopX + i, processedImageAreaLeftTopY + j, inputPixel);
+
+                        // цикл по цвету
+                        for (int k = 0; k < COLORS_COUNT_IN_RGB; ++k) {
+                            intermediateOutputPixel[k] += inputPixel[k] * blurringMatrix[i][j];
                         }
-
-                        pixel[k] = (int) blurredPixel;
                     }
                 }
 
-                // записываем значения цветов для пикселя в картинку
-                rasterOutput.setPixel(x, y, pixel);
+                // покомпонентная запись вычисленных усредненных цветов в результирующий пиксель
+                // с проверкой выхода значений за пределы 0..255
+                for (int k = 0; k < COLORS_COUNT_IN_RGB; ++k) {
+                    outputPixel[k] = (int) intermediateOutputPixel[k];
+
+                    if (outputPixel[k] > 255) {
+                        outputPixel[k] = 255;
+                        continue;
+                    }
+
+                    if (outputPixel[k] < 0) {
+                        outputPixel[k] = 0;
+                    }
+                }
+
+                // записываем пиксель в картинку
+                outputRaster.setPixel(x, y, outputPixel);
             }
         }
 
         // сохраняем картинку в файл
         String fileName = "out_blurred_strength=" + BLURRING_STRENGTH + ".jpg";
-        ImageIO.write(imageOutput, "jpg", new File(fileName));
+        ImageIO.write(outputImage, "jpg", new File(fileName));
     }
 }
