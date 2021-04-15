301e493cc81d16f43b9af97012598e9447534253
Maksim Biluta <biluta.m.a@gmail.com>
2021-04-16 01:44:55 +0200

    new commit

--- src/SQL/join.sql
+++ src/SQL/join.sql
@@ -1,28 +1,49 @@
 USE world;
 
--- #1 код страны, название страны и название города столицы
+-- #1.1 CROSS JOIN таблиц стран и городов.
+-- таблица не имеет смысла, так как все страны сопоставляются со всеми городами без критериев.
+-- Использован фильтр DISTINCT, поскольку одноименные города встречаются в разных странах.
+-- Для примера выбран город Cambridge (есть в Великобритании, США и Канаде) - видим, что сейчас не повторяется.
+SELECT DISTINCT country.name AS countryName, city.name AS cityName
+FROM country
+CROSS JOIN city
+WHERE city.name = 'Cambridge'
+ORDER BY countryName;
+
+-- 1.2 CROSS JOIN для всех уникальных городов
+-- Для примера выбран город Cambridge (есть в Великобритании, США и Канаде) - видим, что сейчас не повторяется.
+SELECT DISTINCT country.name AS countryName, city.name AS cityName
+FROM country
+CROSS JOIN city
+-- WHERE city.name = 'Cambridge'
+ORDER BY countryName, cityName
+LIMIT 10000;
+
+
+-- #2 код страны, название страны и название города столицы
 SELECT country.code, country.name, city.name
 FROM country
 INNER JOIN city
 	ON country.capital = city.id;
 
--- #2 имя города, его численность, код и название его страны
-SELECT city.name, city.population, city.countrycode, country.name
+-- #3 имя города, его численность, код и название его страны
+SELECT city.name AS cityName, city.population, city.countryCode, country.name AS countryName
 FROM city
 INNER JOIN country
-	ON city.countrycode = country.code;
+	ON city.countryCode = country.code;
 
--- #3 количество городов по континентам
-SELECT country.Continent, COUNT(city.name)
+-- #4 количество городов по континентам
+SELECT country.continent, COUNT(city.name)
 FROM country
 LEFT JOIN city
 	ON country.code = city.countryCode
 GROUP BY country.continent;
 
--- #4 количество официальных языков по странам, в порядке убывания
-SELECT country.name, COUNT(countryLanguage.language) as languageCount
-FROM country
-INNER JOIN countrylanguage
-	ON country.code = countryLanguage.countryCode
-GROUP BY countryLanguage.countryCode
+-- #5 количество официальных языков по странам, в порядке убывания
+SELECT country.name AS countryName, COUNT(countryLanguage.language) AS languageCount
+FROM countryLanguage
+INNER JOIN country
+	ON countryLanguage.countryCode = country.code
+WHERE countryLanguage.isOfficial = 'T'
+GROUP BY countryName
 ORDER BY languageCount DESC;
---
+++ src/SQL/test_data.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE shop;
+
+USE shop;
+
+CREATE TABLE categories
+(
+    id INT AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE products
+(
+    id INT AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    price DOUBLE NOT NULL,
+    category_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+        ON UPDATE CASCADE
+);
+
+DELIMITER $$
+CREATE PROCEDURE add_categories()
+BEGIN
+    DECLARE categoriesCount INT DEFAULT 100;
+    DECLARE productsCount INT DEFAULT 5000;
+    DECLARE minimumPrice INT DEFAULT 50;
+    DECLARE maximumPrice INT DEFAULT 500;
+    -- создание категорий
+    DECLARE i INT DEFAULT 1;
+    WHILE i <= categoriesCount DO
+        INSERT INTO categories(name)
+        VALUES (CONCAT('category_', i));
+        SET i = i + 1;
+    END WHILE;
+    -- создание товаров
+    SET i = 1;
+    WHILE i <= productsCount DO
+            INSERT INTO products(name, price, category_id)
+            VALUES (CONCAT('product_', i), FLOOR(RAND() * (maximumPrice - minimumPrice + 1) + minimumPrice),
+                FLOOR(RAND() * categoriesCount + 1));
+            SET i = i + 1;
+    END WHILE;
+END $$
+DELIMITER ;
+
+CALL add_categories();
+
+-- Проверка созданной БД
+SELECT COUNT(name) AS productsCount,
+	MIN(price) AS minPrice,
+    MAX(price) AS maxPrice,
+    MIN(category_id) AS minCatId,
+    MAX(category_id) AS maxCatId
+FROM products;
